85c4fbc3dcd6fe97fbb43f8c99d13173d8de258c
YocoZuna <dawidzadlo12@gmail.com>
2026-06-24 22:31:23 +0200

    Forgot to stage i2c.h

--- Firmware/platform/Inc/i2c.h
+++ Firmware/platform/Inc/i2c.h
@@ -3,14 +3,14 @@
 #include "stm32l4xx_ll_gpio.h"
 #include <stddef.h>
 #include <stdint.h>
-
+#include "time.h"
 
 #define I2C_PORT I2C1
 #define SCL_PIN LL_GPIO_PIN_9
 #define SDA_PIN LL_GPIO_PIN_10
 
 void i2c_init(I2C_TypeDef *I2C, LL_I2C_InitTypeDef *i2c_init_struct);
-void i2c_tx(I2C_TypeDef *I2C,const uint8_t slaveAddr, const uint8_t *data,size_t len);
-void i2c_rx(I2C_TypeDef *I2C,const uint8_t slaveAddr, uint8_t * data, size_t len);
+int8_t i2c_tx(I2C_TypeDef *I2C,const uint8_t slaveAddr, const uint8_t *data,size_t len,uint32_t timeout);
+int8_t i2c_rx(I2C_TypeDef *I2C,const uint8_t slaveAddr, uint8_t * data, size_t len,uint32_t timeout);
 void i2c_deinit(I2C_TypeDef *I2C);
 
