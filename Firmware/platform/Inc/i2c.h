1bdcdb6d358726fda851eda6a6cf3de2ac1092d4
YocoZuna <dawidzadlo12@gmail.com>
2026-07-13 19:37:30 +0200

    I2C rewritten to HAL instead of LL

--- Firmware/platform/Inc/i2c.h
+++ Firmware/platform/Inc/i2c.h
@@ -1,16 +1,26 @@
 #pragma once
-#include "stm32l4xx_ll_i2c.h"
-#include "stm32l4xx_ll_gpio.h"
-#include <stddef.h>
+#include "stm32l4xx_hal_i2c.h"
 #include <stdint.h>
 #include "time.h"
 
+/* TODO this shoould be moved*/
 #define I2C_PORT I2C1
 #define SCL_PIN LL_GPIO_PIN_9
 #define SDA_PIN LL_GPIO_PIN_10
 
-void i2c_init(I2C_TypeDef *I2C, LL_I2C_InitTypeDef *i2c_init_struct);
-int8_t i2c_tx(I2C_TypeDef *I2C,const uint8_t slaveAddr, const uint8_t *data,size_t len,uint32_t timeout);
-int8_t i2c_rx(I2C_TypeDef *I2C,const uint8_t slaveAddr, uint8_t * data, size_t len,uint32_t timeout);
-void i2c_deinit(I2C_TypeDef *I2C);
+/* I2C Status types derived from HAL_StatusTypeDef */
+typedef enum
+{
+  I2C_OK       = 0x00,
+  I2C_ERROR    = 0x01,
+  I2C_BUSY     = 0x02,
+  I2C_TIMEOUT  = 0x03
+} I2C_StatusTypeDef;
+
+I2C_StatusTypeDef i2c_init(I2C_HandleTypeDef *I2C, HAL_I2C_InitTypeDef *i2c_init_struct);
+I2C_StatusTypeDef i2c_tx(I2C_HandleTypeDef *I2C,const uint8_t slaveAddr, const uint8_t *data,size_t len,uint32_t timeout);
+I2C_StatusTypeDef i2c_rx(I2C_HandleTypeDef *I2C,const uint8_t slaveAddr, uint8_t * data, size_t len,uint32_t timeout);
+I2C_StatusTypeDef i2c_write_mem(I2C_HandleTypeDef *I2C, const uint8_t slaveAddr, const uint16_t memAddr, const uint8_t *data, size_t len, uint32_t timeout);
+I2C_StatusTypeDef i2c_read_mem(I2C_HandleTypeDef *I2C, const uint8_t slaveAddr, const uint16_t memAddr, uint8_t *data, size_t len, uint32_t timeout);
+I2C_StatusTypeDef i2c_deinit(I2C_HandleTypeDef *I2C);
 
--- Firmware/platform/Src/i2c.c
+++ Firmware/platform/Src/i2c.c
@@ -1,112 +1,28 @@
 #include "i2c.h"
-#include "stm32l4xx_ll_bus.h"
-#include "stm32l4xx_ll_i2c.h"
-#include <sys/types.h>
 
-
-
-void i2c_init(I2C_TypeDef *I2C, LL_I2C_InitTypeDef *i2c_init_struct)
+I2C_StatusTypeDef i2c_init(I2C_HandleTypeDef *I2C, HAL_I2C_InitTypeDef *i2c_init_struct)
 {
-    LL_I2C_Disable(I2C);
-    if (I2C == I2C1) LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_I2C1);
-    LL_I2C_Init(I2C, i2c_init_struct);
-    LL_I2C_Enable(I2C);
+    return HAL_I2C_Init(I2C, i2c_init_struct);
 }
-
-static int8_t i2c_abort(I2C_TypeDef *I2C, uint32_t timeout) {
-    
-    LL_I2C_GenerateStopCondition(I2C);
-    uint32_t start_time = getTime();
-    while (!LL_I2C_IsActiveFlag_STOP(I2C)) {
-        if (time_expired(&start_time, timeout)) {
-            return -3; // Timeout
-        }
-    }
-    LL_I2C_ClearFlag_NACK(I2C);
-    LL_I2C_ClearFlag_STOP(I2C);
-    return 1;
-}
-
-int8_t i2c_tx(I2C_TypeDef *I2C,const uint8_t slaveAddr, const uint8_t *data,size_t len,uint32_t timeout){
-    
-    uint32_t start_time = getTime();
-    
-    while(LL_I2C_IsActiveFlag_BUSY(I2C)){
-        if(time_expired(&start_time, timeout)) {
-            return -3; 
-        }
-    };
-    LL_I2C_HandleTransfer(I2C, slaveAddr<<1,LL_I2C_ADDRSLAVE_7BIT, len,LL_I2C_MODE_AUTOEND, LL_I2C_GENERATE_START_WRITE);
-    
-    for(size_t i=0;i<len;i++){
-        start_time = getTime();
-        if(data==NULL && len>0) return -5; // Avoid sending 0x00 as it can be interpreted as a NACK in some cases
-        
-        while(!LL_I2C_IsActiveFlag_TXIS(I2C)){
-            //Check if NACK was received
-            if(LL_I2C_IsActiveFlag_NACK(I2C)){
-                i2c_abort(I2C,timeout); // Abort the transfer
-                return -1; // NACK received
-            }
-            if(time_expired(&start_time, timeout)) {
-                i2c_abort(I2C,timeout); // Timeout
-                return -3;
-            }
-        }
-        LL_I2C_TransmitData8(I2C, data[i]);
-    }
-    start_time = getTime();
-    while(!LL_I2C_IsActiveFlag_STOP(I2C)){
-        if(time_expired(&start_time, timeout)) {
-                i2c_abort(I2C,timeout); // Timeout
-                return -3;
-            }
-    }
-    LL_I2C_ClearFlag_STOP(I2C);
-
-    return 1;
-}
-
-int8_t i2c_rx(I2C_TypeDef *I2C,const uint8_t slaveAddr, uint8_t * data, size_t len,uint32_t timeout){
-    uint32_t start_time = getTime();
-    while(LL_I2C_IsActiveFlag_BUSY(I2C)){
-        if(time_expired(&start_time, timeout)) {
-            return -3; 
-        }
-    }
-    LL_I2C_HandleTransfer(I2C, slaveAddr<<1,LL_I2C_ADDRSLAVE_7BIT, len,LL_I2C_MODE_AUTOEND, LL_I2C_GENERATE_START_READ);
-    for(size_t i=0;i<len;i++){
-        start_time = getTime();
-        while(!LL_I2C_IsActiveFlag_RXNE(I2C)){
-                //Check if NACK was received
-                if(LL_I2C_IsActiveFlag_NACK(I2C)){
-                    i2c_abort(I2C,timeout); // Abort the transfer
-                    return -1; // NACK received
-                }
-                if(time_expired(&start_time, timeout)) {
-                    i2c_abort(I2C,timeout); // Timeout
-                    return -3;
-                }
-            }
-        
-        data[i] = LL_I2C_ReceiveData8(I2C);
-    }
-    
-    start_time = getTime();
-    while(!LL_I2C_IsActiveFlag_STOP(I2C)){
-        if(time_expired(&start_time, timeout)) {
-                i2c_abort(I2C,timeout); // Timeout
-                return -3;
-            }
-    };
-    LL_I2C_ClearFlag_STOP(I2C);
-
-    return 1;
-}
-
-void i2c_deinit(I2C_TypeDef *I2C)
+I2C_StatusTypeDef i2c_tx(I2C_HandleTypeDef *I2C,const uint8_t slaveAddr, const uint8_t *data,size_t len,uint32_t timeout)
 {
-    if (I2C == I2C1) LL_APB1_GRP1_DisableClock(LL_APB1_GRP1_PERIPH_I2C1);
-    LL_I2C_Disable(I2C);
+    return HAL_I2C_Master_Transmit(I2C, slaveAddr, data, len, timeout);
 }
+I2C_StatusTypeDef i2c_rx(I2C_HandleTypeDef *I2C,const uint8_t slaveAddr, uint8_t * data, size_t len,uint32_t timeout){
+
+    return HAL_I2C_Master_Receive(I2C, slaveAddr, data, len, timeout);
+}
+I2C_StatusTypeDef i2c_write_mem(I2C_HandleTypeDef *I2C, const uint8_t slaveAddr, const uint16_t memAddr, const uint8_t *data, size_t len, uint32_t timeout){
+    
+    return HAL_I2C_Mem_Write(I2C, slaveAddr, memAddr, I2C_MEMADD_SIZE_16BIT, data, len, timeout);
+}
+I2C_StatusTypeDef i2c_read_mem(I2C_HandleTypeDef *I2C, const uint8_t slaveAddr, const uint16_t memAddr, uint8_t *data, size_t len, uint32_t timeout){
+    
+    return HAL_I2C_Mem_Read(I2C, slaveAddr, memAddr, I2C_MEMADD_SIZE_16BIT, data, len, timeout);
+}
+I2C_StatusTypeDef i2c_deinit(I2C_HandleTypeDef *I2C)
+{
+    return HAL_I2C_DeInit(I2C);
+}
+
 
